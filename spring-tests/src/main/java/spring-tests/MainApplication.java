de7882872cbefc345fe61bb5bc754cfca3e1aa6c
Timo H. Geusch <timo-git@unix-consult.com>
2017-05-01 17:21:10 -0700

    Got the basic files to build

---
+++ spring-tests/src/main/java/spring-tests/MainApplication.java
@@ -0,0 +1,20 @@
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication implements CommandLineRunner {
+
+    //@Autowired
+    //private CustomerRepository repository;
+
+    public static void main(String[] args) {
+	SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
