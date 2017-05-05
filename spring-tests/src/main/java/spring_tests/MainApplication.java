1f33fe741fc2f37e5ae0a0ecfe2a0c48564b0941
Timo Geusch <timo.geusch@mongodb.com>
2017-05-04 20:46:11 -0700

    Added test using Query/UpdateFirst

--- spring-tests/src/main/java/spring_tests/MainApplication.java
+++ spring-tests/src/main/java/spring_tests/MainApplication.java
@@ -4,13 +4,19 @@ import java.lang.System;
 import java.io.IOException;
 import java.util.LongSummaryStatistics;
 import java.util.List;
-import java.util.Vector;
+import java.util.ArrayList;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 @SpringBootApplication
 public class MainApplication implements CommandLineRunner {
@@ -32,20 +38,23 @@ public class MainApplication implements CommandLineRunner {
 	runInsertTests();
 	System.console().printf("Starting update via save tests\n");
 	runUpdateViaSaveTests();
+	System.console().printf("Starting update via updateFirst() tests\n");
+	runUpdateViaSingleUpdateTests();
 	runReadTests();
 	runDeleteTests();
     }
 
     private void initialiseTests() throws IOException {
 	System.console().printf("Initializing tests\n");
+	measuredTimes = new ArrayList<Long>(100001);
 	docgenL = new LargeDocGenerator("metamorphosis-kafka.txt");
 	largeDocs.deleteAll();
 	System.console().printf("Done initializing tests\n");
     }
     
     private void runInsertTests() {
-	Vector<Long> measuredTimes = new Vector<>(100001);
-	long totalTime = 0;
+	measuredTimes.clear();
+
 	// Large docs first
 	System.console().printf("Begin insert tests\n");
 	for (int i = 0; i < 100001; i++) {
@@ -65,12 +74,13 @@ public class MainApplication implements CommandLineRunner {
 	}
 	System.console().printf("Total execution time (ms) : %d\n", stats.getSum() / 1000);
 	System.console().printf("Average insert time (ms): %f\n", stats.getAverage() / 1000);
-	System.console().printf("Median insert time (ms): %d\n", measuredTimes.elementAt(100001/2) / 1000);
+	System.console().printf("Median insert time (ms): %d\n", measuredTimes.get(100001/2) / 1000);
     }
 
     private void runUpdateViaSaveTests() {
-	Vector<Long> measuredTimes = new Vector<>(100001);
-	System.console().printf("Begin update tests\n");
+	measuredTimes.clear();
+	
+	System.console().printf("Begin update via save tests\n");
 	List<LargeDoc> ldocs = largeDocs.findByTestStringField("a");
 	for (LargeDoc doc : ldocs) {
 	    doc.lotsOfStuff.add("or palace,");
@@ -95,9 +105,44 @@ public class MainApplication implements CommandLineRunner {
 	}
 	System.console().printf("Total   update via save execution time (ms): %d\n", stats.getSum() / 1000);
 	System.console().printf("Average update via save insert time (ms): %f\n", stats.getAverage() / 1000);
-	System.console().printf("Median  update via save insert time (ms): %d\n", measuredTimes.elementAt(measuredTimes.size()/2) / 1000);
+	System.console().printf("Median  update via save insert time (ms): %d\n", measuredTimes.get(measuredTimes.size()/2) / 1000);
     }
 
+    private void runUpdateViaSingleUpdateTests() {
+	System.console().printf("Begin update via updateFirst tests\n");
+	
+	measuredTimes.clear();
+	ApplicationContext ctx =  new AnnotationConfigApplicationContext(BasicMongoConfiguration.class);
+	MongoOperations mongoOp = (MongoOperations)ctx.getBean("mongoTemplate");
+
+	List<LargeDoc> ldocs = largeDocs.findByTestStringField("b");
+	for (LargeDoc doc : ldocs) {
+	    Query q = new Query();
+	    q.addCriteria(Criteria.where("_id").is(doc.Id));
+	    Update up = new Update();
+	    up.push("lotsOfStuff", "The");
+	    up.push("lotsOfStuff", "joy");
+	    up.push("lotsOfStuff", "of king");
+	    up.push("lotsOfStuff", "and");
+	    up.push("lotsOfStuff", "retainers is,");
+	    up.push("lotsOfStuff", "however,");
+	    up.push("lotsOfStuff", "of");
+	    up.push("lotsofStuff", "short duration.");
+	    long startNS = System.nanoTime();
+	    mongoOp.updateFirst(q, up, LargeDoc.class);
+	    long endNS   = System.nanoTime();
+	    measuredTimes.add(endNS - startNS);
+	}
+	Collections.sort(measuredTimes);
+	LongSummaryStatistics stats = new LongSummaryStatistics();
+	for (long v : measuredTimes) {
+	    stats.accept(v);
+	}
+	System.console().printf("Total   update via update execution time (ms): %d\n", stats.getSum() / 1000);
+	System.console().printf("Average update via update insert time (ms): %f\n", stats.getAverage() / 1000);
+	System.console().printf("Median  update via update insert time (ms): %d\n", measuredTimes.get(measuredTimes.size()/2) / 1000);
+    }
+    
     private void runReadTests() {
     }
 
@@ -105,4 +150,5 @@ public class MainApplication implements CommandLineRunner {
     }
 
     private LargeDocGenerator docgenL;
+    private ArrayList<Long> measuredTimes;
 }
